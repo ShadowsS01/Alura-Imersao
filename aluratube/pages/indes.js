4985b5ec5cc2d86e074fac0aa90bdc54609952a6
DKSHS <88355080+ShadowsS01@users.noreply.github.com>
2022-11-11 10:59:45 -0300

    Aluratube: Aula 1

--- aluratube/pages/indes.js
+++ aluratube/pages/indes.js
@@ -1,3 +1,95 @@
+import Head from "next/head";
+import config from "../config.json";
+import styled from "styled-components";
+import { CSSReset } from "../src/components/CSSReset";
+import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/Timeline";
+
 export default function HomePage() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Head>
+        <title>AluraTube</title>
+        <link rel="icon" href="/Logo.svg" />
+      </Head>
+      <CSSReset />
+      <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
+        <Menu />
+        <Header />
+        <Timeline playlists={config.playlists}>Conteúdo</Timeline>
+      </div>
+    </>
+  );
+}
+
+const StyledHeader = styled.div`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+  .user-info {
+    margin-top: 50px;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 16px 32px;
+    gap: 16px;
+  }
+  a {
+    border: none;
+    color: ${({ theme }) => theme.borderBase || "rgba(24, 24, 27, 1)"};
+    &:hover,
+    &:focus-visible {
+      text-decoration: underline;
+    }
+  }
+`;
+function Header() {
+  return (
+    <StyledHeader>
+      {/* <img src="banner" /> */}
+      <section className="user-info">
+        <a target="_blank" href={`https://github.com/${config.github}`}>
+          <img src={`https://github.com/${config.github}.png`} />
+        </a>
+        <div>
+          <h2>
+            <a target="_blank" href={`https://github.com/${config.github}`}>
+              {config.name}
+            </a>
+          </h2>
+          <p>{config.job}</p>
+        </div>
+      </section>
+    </StyledHeader>
+  );
+}
+
+function Timeline(propriedades) {
+  const playlistNames = Object.keys(propriedades.playlists);
+
+  return (
+    <StyledTimeline>
+      {playlistNames.map((playlistName) => {
+        const videos = propriedades.playlists[playlistName];
+
+        return (
+          <section key={playlistName}>
+            <h2>{playlistName}</h2>
+            <div>
+              {videos.map((video) => {
+                return (
+                  <a href={video.url} key={video.url}>
+                    <img src={video.thumb} />
+                    <span>{video.title}</span>
+                  </a>
+                );
+              })}
+            </div>
+          </section>
+        );
+      })}
+    </StyledTimeline>
+  );
 }
---
+++ aluratube/src/Menu.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { Logo } from "./Logo";
+
+const StyledMenu = styled.header`
+  z-index: 9999;
+  display: flex;
+  flex-direction: row;
+  height: 56px;
+  justify-content: space-between;
+  background-color: ${({ theme }) =>
+    theme.backgroundLevel1 || "rgba(255, 255, 255, 0.85)"};
+  backdrop-filter: blur(8px);
+  border: 1px solid ${({ theme }) => theme.borderBase || "#e5e5e5"};
+  align-items: center;
+  padding: 0 16px;
+  gap: 16px;
+  position: fixed;
+  width: 100%;
+  .logo {
+    width: 100%;
+    max-width: 80px;
+    @media (min-width: 600px) {
+      max-width: 127px;
+    }
+    .text {
+      fill: ${({ theme }) => theme.textColorBase || "#222222"};
+    }
+  }
+`;
+
+export default function Menu() {
+  return (
+    <StyledMenu>
+      <a href="/">
+        <Logo />
+      </a>
+    </StyledMenu>
+  );
+}
